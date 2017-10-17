6f9b36cb6bd590df5c5f80f0aefc92fbaafe1eb1
Kiernan Callaghan <kiernan.calla@gmail.com>
2017-10-17 12:13:33 +0100

    Started map!

---
+++ map.py
@@ -0,0 +1,28 @@
+from items import *
+
+room_1 = {
+    "name": "Labyrinth Entrance",
+
+    "description":
+    """""",#tbd
+
+    "exits": {"north": "room_2"},
+
+    "items": [item_biscuits, item_handbook], #tbd
+
+    "co-ordinates": [0, 0],
+
+    "monster": "",
+
+    "entered": True,
+
+    "interactables": "",
+
+    "used": False,
+    
+}
+
+rooms = {
+    "Labyrinth Entrance": room_1,
+    
+}
