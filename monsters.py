73bc26ec4585f97419a2feb61dc4a5e3ab73081f
jwstevenson <jwstevenson07@gmail.com>
2017-10-23 15:48:34 +0100

    Added more monsters

--- monsters.py
+++ monsters.py
@@ -21,8 +21,162 @@ goblin = {  # Sample monster subject to change.
 
     "loot": [],
 
+    "experience": 20
+}
+
+skeleton = {
+    "name": "Skeleton",
+
+    "description": "",
+
+    "level": 1,
+
+    "max health": 8,
+
+    "status effects": [],
+
+    "attacks": {
+        "scratch": attacks.scratch_attack,
+        "stun": attacks.stun_attack
+    },
+
+    "loot": [],
+
+    "experience": 15
+}
+
+slime = {
+    
+    "name": "Slime",
+
+    "description": "",
+
+    "level": 1,
+
+    "max health": 5,
+
+    "status_effects": [],
+
+    "attacks": {
+        "scratch": attacks.scratch_attack
+    },
+
+    "loot": [],
+
     "experience": 10
 }
 
+zombie = {
+    
+    "name": "Zombie",
+
+    "description": "",
+
+    "level": 2,
+
+    "max health": 20,
+
+    "status_effects": [],
+
+    "attacks": {
+        "punch": attacks.punch_attack
+    },
+
+    "loot": [],
+
+    "experience": 25
+}
+
+orc = {
+    
+    "name": "ORc",
+
+    "description": "",
+
+    "level": 2,
+
+    "max health": 12,
+
+    "status_effects": [],
+
+    "attacks": {
+        "punch": attacks.punch_attack,
+        "sword": attacks.sword_attack
+    },
+
+    "loot": [],
+
+    "experience": 30
+}
+
+ogre = {
+    
+    "name": "Ogre",
+
+    "description": "",
+
+    "level": 2,
+
+    "max health": 20,
+
+    "status_effects": [],
+
+    "attacks": {
+        "scratch": attacks.scratch_attack,
+        "stun": attacks.stun_attack,
+        "punch": attacks.punch_attack
+    },
+
+    "loot": [],
+
+    "experience": 35
+}
+
+evil_unicorn = {
+    
+    "name": "Evil Unicorn"
+
+    "description": "",
+
+    "level": 3,
+
+    "max health": 25,
+
+    "status_effects": [],
+
+    "attacks": {
+        "punch": attacks.punch_attack
+    },
+
+    "loot": [],
+
+    "experience": 30
+}
+
+gang_of_bandits = {
+    
+    "name": "Gang of Bandits",
+
+    "description": "",
+
+    "level": 3,
+
+    "max health": 45,
+
+    "status_effects": [],
+
+    "attacks": {
+        "punch": attacks.punch_attack,
+        "sword": attacks.sword_attack
+
+    },
+
+    "loot": [],
+
+    "experience": 60
+}
+
+
+
 # Important! In order for optimization to be implemented later on, keep the below list ordered by level.
-monsters = [goblin]
+monsters = [goblin, skeleton, slime, zombie, orc, ogre, evil_unicorn, gang_of_bandits]
