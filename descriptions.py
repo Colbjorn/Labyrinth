779cb552f95027da260267b08b1dd15ed4eea53c
KayaJe <32775549+KayaJe@users.noreply.github.com>
2017-10-24 11:14:42 +0100

    Add files via upload

--- descriptions.py
+++ descriptions.py
@@ -1,1 +1,97 @@
 # This module lists room descriptions because we really need these in their own file.
+
+room_hell = {
+    "name": "Hell"
+    
+    "description":
+    """You are surrounded by fire and you are now trapped inside.
+       Next to you, you see a skeleton holding a burning hammer.
+       Then you see a old and dusty table which has the golden key
+       with a poisonous spider. You only have one way to get out of
+       this place safely. However the door is locked. To get out of
+       this place safely, you will need to pick either the hammer
+       to break the door and exit to the dinning hall or the key to
+       unlock the door and exit to the classroom.""",
+
+}
+
+room_classroom = {
+    "name": "classoom",
+
+    "description":
+    """You are in a classroom and you see the room flooded. Beside you,
+       you see Kirill in a statue pointing at someone. To the north is
+       the door which leads to death room and to the south is
+       the dark and stormtrooper."""
+}
+
+room_death = {
+    "name": "Death room"
+
+    "description":
+    """You are sitting on a old, dusty and stiff chair. Straight ahead
+       you can see dead bodies with full of blood. The walls are all
+       painted with blood. A toy is just approaching you by making a
+       sound. You turn to your right and you see a door which has two routes.
+        """
+}
+#Sound to be added
+
+room_dark_and_stormtrooper = {
+    "name": "Dark and stormtrooper"
+
+    "description"
+    """You are now sitting in a bath tub with the deadly stormtroopers.
+       You can see numerous of them staring at you but won't communicate
+       with you. The room is very dark that you can't even see the bath tub
+       properly. There's a small candle on the table next to the bath tub.
+       The flame barely flickered which makes it easier for you to see the
+       stormtroopers."""
+       
+}
+
+room_forest = {
+    "name": "Forest room"
+    "description"
+    """You are standing in the dark forest and all you can see are the green
+       trees. However as you walk forward the trees look burnt and the ground
+       is full of red leaves.
+       """
+}
+
+room_skeleton = {
+    "name": "Skeleton room"
+    "description"
+    """You are leaning on a skeleton and you see white smokes coming out from
+       a window next to you. As it fades you can then see more skeleton hanging
+       on a monkey bar.  """
+}
+
+room_mountain = {
+    "name": "Mountain lab"
+    "description"
+    """You are standing next to the mountain. The room is full of black snow mixed
+       with blood. Then you see a man looking at you then ignores you and walk. """
+}
+room_acid = {
+    "name": "Acid room"
+    "descripttion"
+    """
+
+}
+
+
+
+rooms = {
+    "Hall": room_halls,
+    "
+}
+
+
+#dark and stormtrooper
+#Death room
+#Skeleton room
+#Deadly Witch room
+#Dinning hall
+#forest room
+#t
