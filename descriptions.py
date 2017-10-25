6dcbf2a8b2688256126877c1244736377d117643
jwstevenson <jwstevenson07@gmail.com>
2017-10-25 11:39:14 +0100

    Fixed indentation problem

--- descriptions.py
+++ descriptions.py
@@ -4,9 +4,9 @@ room_hell = {
     "name": "Hell",
     
     "description":
-    """You are surrounded by fire.
-       Next to you, you see a skeleton holding a burning hammer.
-       You see an old and dusty table which has a golden key on top.""",
+"""You are surrounded by fire.
+Next to you, you see a skeleton holding a burning hammer.
+You see an old and dusty table which has a golden key on top.""",
 
 }
 
@@ -14,17 +14,17 @@ room_classroom = {
     "name": "Classroom",
 
     "description":
-    """You are in a flooded classroom. Beside you,
-       you see a statue of Kirill pointing at someone."""
+"""You are in a flooded classroom. Beside you,
+you see a statue of Kirill pointing at someone."""
 }
 
 room_death = {
     "name": "Death room",
 
     "description":
-    """You are sitting on a old, dusty and stiff chair. Straight ahead
-       you can see dead bodies. The walls are
-       painted with blood. You turn to your right and you see a door which has two routes.
+"""You are sitting on a old, dusty and stiff chair. Straight ahead
+you can see dead bodies. The walls are
+painted with blood. You turn to your right and you see a door which has two routes.
         """
 }
 #Sound to be added
@@ -33,12 +33,12 @@ room_dark_and_stormtrooper = {
     "name": "Dark and stormy",
 
     "description":
-    """You enter a dark room. Infront of you is an old rusty bathtub containing 
-       suspicious men with white helmets.
-       They seem to be looking in your direction.
-       There's a small candle on the table next to the bath tub.
-       The flame gently flickers, allowing short glimpses of the
-       shiny white helmets."""
+"""You enter a dark room. Infront of you is an old rusty bathtub containing 
+suspicious men with white helmets.
+They seem to be looking in your direction.
+There's a small candle on the table next to the bath tub.
+The flame gently flickers, allowing short glimpses of the
+shiny white helmets."""
        
 }
 
@@ -46,9 +46,9 @@ room_forest = {
     "name": "Forest room",
 
     "description":
-    """You are standing in a dark forest surrounded by the tall trees.
-       As you walk forward you notice that the trees look burnt and the ground
-       is warm and full of ashes.
+"""You are standing in a dark forest surrounded by the tall trees.
+As you walk forward you notice that the trees look burnt and the ground
+is warm and full of ashes.
        """
 }
 
@@ -56,48 +56,48 @@ room_skeleton = {
     "name": "Skeleton room",
 
     "description":
-    """You enter a cold, dark room. You spot a skinny figure leaning against the stone walls, 
-       a tiny puff of smoke enters the room from a thin gap in the floorboads.
-       As it fades you notice that the floor is damp and full of bones.  """
+"""You enter a cold, dark room. You spot a skinny figure leaning against the stone walls, 
+a tiny puff of smoke enters the room from a thin gap in the floorboads.
+As it fades you notice that the floor is damp and full of bones.  """
 }
 
 room_mountain = {
     "name": "Mountain",
 
     "description":
-    """You are standing next to a mountain. It is gently snowing and the light from the sun pierces your eyes.
-       You look down and notice red spots in the snow, and a long track from something that has 
-       been dragged through it. In the distance you spot two red glimmers that look like eyes. """
+"""You are standing next to a mountain. It is gently snowing and the light from the sun pierces your eyes.
+You look down and notice red spots in the snow, and a long track from something that has 
+been dragged through it. In the distance you spot two red glimmers that look like eyes. """
 }
 room_acid = {
     "name": "Acid room",
     "description":
-    """You are leaning against the wall and you see a door which has a lot of smoke
-       flowing out from underneath it. You see a man in a white jacket with a burnt face holding a 
-       bottle of clear liquid looking at you. """
+"""You are leaning against the wall and you see a door which has a lot of smoke
+flowing out from underneath it. You see a man in a white jacket with a burnt face holding a 
+bottle of clear liquid looking at you. """
 
 }
 
 room_disco = {
     "name": "Disco",
     "description":
-    """You find yourself on a sweaty dancefloor and you can't help but get down to the funky music.
-       Loving life, you look to your right and notice that you are surrounded by tall, hairy creatures 
-       and they are not in the mood to boogey. """
+"""You find yourself on a sweaty dancefloor and you can't help but get down to the funky music.
+Loving life, you look to your right and notice that you are surrounded by tall, hairy creatures 
+and they are not in the mood to boogey. """
 }
 
 room_ocean = {
     "name": "Ocean room",
     "description":
-    """You are in a boat which is in the middle of the ocean. The sea becomes turbulant and the sky turns black.
-       You grip the sides of the boat and pull yourself through a hatch on the deck where you find a musty dark room.   """
+"""You are in a boat which is in the middle of the ocean. The sea becomes turbulant and the sky turns black.
+You grip the sides of the boat and pull yourself through a hatch on the deck where you find a musty dark room.   """
 }
 
 room_darkness = {
     "name": "Darkness",
     "description":
-    """Your vision is heavily impaired. Are you going blind? You reach out infront of you and feel around. 
-       You feel a sharp pain on your finger tip and a small trickle of blood slides down your hand.  """
+"""Your vision is heavily impaired. Are you going blind? You reach out infront of you and feel around. 
+You feel a sharp pain on your finger tip and a small trickle of blood slides down your hand.  """
 }
 
 
